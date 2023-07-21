a5c518490e71e702a33742d84f925be4b4d185d5
sigleto <sigleto@gmail.com>
2023-07-21 18:33:08 +0200

    mejoras

--- componentes/Bares.js
+++ componentes/Bares.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, StyleSheet,Linking } from "react-native";
+import { View, Text, StyleSheet,Linking,ScrollView } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
 
 const Bares = () => {
@@ -7,7 +7,8 @@ const Bares = () => {
     Linking.openURL(url);
   };
   return (
-    <View style={styles.container}>
+    <ScrollView contentContainerStyle={styles.container}>
+    
       <View style={styles.estab}>
         <Text style={styles.bar}>LA PARRALA</Text>
         <TouchableOpacity onPress={() => handleLinkPress("https://www.google.es/maps/place/La+Parrala/@37.3824545,-6.0019085,15z/data=!4m5!3m4!1s0x0:0xd99fca132859003!8m2!3d37.3824545!4d-6.0019085")}>
@@ -21,20 +22,20 @@ const Bares = () => {
         </TouchableOpacity>
       </View>
       <View style={styles.estab}>
-        <Text style={styles.bar}>LAS GOLONDRINAS 2</Text>
-        <TouchableOpacity onPress={() => handleLinkPress("https://www.google.es/maps/place/Las+Golondrinas+-+Pag%C3%A9s+del+Corro/@37.3840763,-6.0055876,15z/data=!4m5!3m4!1s0x0:0x9532523f76e64bd0!8m2!3d37.3840811!4d-6.0055642")}>
+        <Text style={styles.bar}>LAS GOLONDRINAS</Text>
+        <TouchableOpacity onPress={() => handleLinkPress("https://www.google.es/search?tbs=lf:1,lf_ui:9&tbm=lcl&sxsrf=AB5stBgrAasCeWLoosit2qYfXfxnOIP7Yw:1689944126909&q=las+golondrinas+2&rflfq=1&num=10&rldimm=10750745693018016720&ved=2ahUKEwjxhf-67J-AAxWei_0HHdolA7QQu9QIegQIEhAM#rlfi=hd:;si:10750745693018016720;mv:[[37.384902,-6.0055684],[37.3840296,-6.0055887]];tbs:lrf:!1m4!1u3!2m2!3m1!1e1!1m4!1u2!2m2!2m1!1e1!2m1!1e2!2m1!1e3!3sIAE,lf:1,lf_ui:9")}>
           <Text style={styles.link}>Ubicación</Text>
         </TouchableOpacity>
-        <TouchableOpacity onPress={() => handleLinkPress("")}>
-          <Text style={styles.link}>Reserva una mesa</Text>
+        <TouchableOpacity >
+          <Text style={styles.link}>Reserva: 954 33 36 40</Text>
         </TouchableOpacity>
-        <TouchableOpacity onPress={() => handleLinkPress("https://www.aeat/wlpl/RHCS-JDIT/HorarioOVP")}>
+        <TouchableOpacity onPress={() => handleLinkPress("https://www.tripadvisor.es/Restaurant_Review-g187443-d2360731-Reviews-Blanca_Paloma-Seville_Province_of_Seville_Andalucia.html")}>
           <Text style={styles.link}>Estrellas de TripAdvisor</Text>
         </TouchableOpacity>
       </View>
-      <View style={styles.estab}>
+    <View style={styles.estab}>
         <Text style={styles.bar}>EL ALBOROQUE</Text>
-        <TouchableOpacity onPress={() => handleLinkPress("https://github.com/sigleto/paginaweb.git")}>
+        <TouchableOpacity >
           <Text style={styles.link}>Ubicación</Text>
         </TouchableOpacity>
         <TouchableOpacity onPress={() => handleLinkPress("https://www.google.com/maps/reserve/v/dine/c/nqeAimyFvos?source=pa&hl=es-ES&gei=epXqY8raCoSCkdUPoI6N2A8&sourceurl=http://www.google.es/service/MapsPlaceService/GetPlace?authuser%3D0%26gl%3Des%26hl%3Des%26q%3DAlboroque%2B,Tapas,%2BBar%2BRestaurantes")}>
@@ -44,7 +45,128 @@ const Bares = () => {
           <Text style={styles.link}>Estrellas de TripAdvisor</Text>
         </TouchableOpacity>
       </View>
-    </View>
+    <View style={styles.estab}>
+        <Text style={styles.bar}>LA BLANCA PALOMA</Text>
+        <TouchableOpacity onPress={() => handleLinkPress("https://www.google.es/search?tbs=lf:1,lf_ui:1&tbm=lcl&sxsrf=AB5stBhaEV9oxupxv3Iulg7KLLqQd3zcVA:1689944456361&q=la+blanca+paloma&rflfq=1&num=10&rldimm=9096604268979346244&ved=2ahUKEwjqtYvY7Z-AAxUKUKQEHRArCrQQu9QIegQIERAM#rlfi=hd:;si:9096604268979346244;mv:[[37.425395699999996,-5.9018292],[37.3807165,-6.0595773]];tbs:lrf:!1m4!1u3!2m2!3m1!1e1!1m4!1u2!2m2!2m1!1e1!2m1!1e2!2m1!1e3!3sIAE,lf:1,lf_ui:1")}>
+          <Text style={styles.link}>Ubicación</Text>
+        </TouchableOpacity>
+        <TouchableOpacity >
+          <Text style={styles.link}>Reserva: 954 33 36 40</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => handleLinkPress("https://www.tripadvisor.es/Restaurant_Review-g187443-d2360731-Reviews-Blanca_Paloma-Seville_Province_of_Seville_Andalucia.html")}>
+          <Text style={styles.link}>Estrellas de TripAdvisor</Text>
+        </TouchableOpacity>
+      </View>
+    <View style={styles.estab}>
+        <Text style={styles.bar}>CASA DIEGO</Text>
+        <TouchableOpacity >
+          <Text style={styles.link}>Ubicación</Text>
+        </TouchableOpacity>
+        <TouchableOpacity >
+          <Text style={styles.link}>No se reserva</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => handleLinkPress("https://www.tripadvisor.co/Restaurant_Review-g187443-d6778587-Reviews-or50-Casa_Diego-Seville_Province_of_Seville_Andalucia.html")}>
+          <Text style={styles.link}>Estrellas de TripAdvisor</Text>
+        </TouchableOpacity>
+      </View>
+    <View style={styles.estab}>
+        <Text style={styles.bar}>CASA RUPERTO</Text>
+        <TouchableOpacity onPress={() => handleLinkPress("https://www.google.es/maps/place/Bar+Casa+Ruperto/@37.3819311,-6.0085324,15z/data=!4m6!3m5!1s0xd126c6963bc510d:0xc6d653b3474cfdfc!8m2!3d37.3819311!4d-6.0085324!16s%2Fg%2F1tznnlzk?entry=ttu")}>
+          <Text style={styles.link}>Ubicación</Text>
+        </TouchableOpacity>
+        <TouchableOpacity >
+          <Text style={styles.link}>No se reserva</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => handleLinkPress("https://www.tripadvisor.es/Restaurant_Review-g187443-d4040632-Reviews-Bar_Casa_Ruperto-Seville_Province_of_Seville_Andalucia.html")}>
+          <Text style={styles.link}>Estrellas de TripAdvisor</Text>
+        </TouchableOpacity>
+      </View>
+    <View style={styles.estab}>
+        <Text style={styles.bar}>POR LA BOCA MUERE EL PEZ</Text>
+        <TouchableOpacity onPress={() => handleLinkPress("https://www.google.es/maps/place/Restaurante+Por+la+boca+muere+el+pez+tapas/@37.3766714,-6.005816,15z/data=!4m2!3m1!1s0x0:0xbb965783ee86fad6?sa=X&ved=2ahUKEwiLlI6pppP9AhXQSKQEHbfFB5YQ_BJ6BQiHARAI")}>
+          <Text style={styles.link}>Ubicación</Text>
+        </TouchableOpacity>
+        <TouchableOpacity >
+          <Text style={styles.link}>Reserva: 625 81 99 34</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => handleLinkPress("https://www.tripadvisor.es/Restaurant_Review-g187443-d23375731-Reviews-Por_La_Boca_Muere_El_Pez_Tapas-Seville_Province_of_Seville_Andalucia.html")}>
+          <Text style={styles.link}>Estrellas de TripAdvisor</Text>
+        </TouchableOpacity>
+      </View>
+    <View style={styles.estab}>
+        <Text style={styles.bar}>VEGA 10</Text>
+        <TouchableOpacity >
+          <Text style={styles.link}>Ubicación</Text>
+        </TouchableOpacity>
+        <TouchableOpacity >
+          <Text style={styles.link}>Reserva una mesa</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => handleLinkPress("https://www.tripadvisor.es/Restaurant_Review-g187443-d1738841-Reviews-Vega_10-Seville_Province_of_Seville_Andalucia.html")}>
+          <Text style={styles.link}>Estrellas de TripAdvisor</Text>
+        </TouchableOpacity>
+      </View>
+    <View style={styles.estab}>
+        <Text style={styles.bar}>LA COARTADA</Text>
+        <TouchableOpacity onPress={() => handleLinkPress("https://www.tripadvisor.es/Restaurant_Review-g187443-d23934279-Reviews-La_Coartada-Seville_Province_of_Seville_Andalucia.html")}>
+          <Text style={styles.link}>Ubicación</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => handleLinkPress("https://www.thefork.es/restaurante/la-coartada-r721345#date=2023-07-13&hour=1290&partySize=2")}>
+          <Text style={styles.link}>Reserva una mesa</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => handleLinkPress("https://www.tripadvisor.es/Restaurant_Review-g187443-d23934279-Reviews-La_Coartada-Seville_Province_of_Seville_Andalucia.html")}>
+          <Text style={styles.link}>Estrellas de TripAdvisor</Text>
+        </TouchableOpacity>
+      </View>
+    <View style={styles.estab}>
+        <Text style={styles.bar}>GASCONA</Text>
+        <TouchableOpacity onPress={() => handleLinkPress("https://www.google.es/maps/place/Gascona/@37.3867464,-6.0048046,15z/data=!4m2!3m1!1s0x0:0x4ec5e366e3ccb3fa?sa=X&ved=2ahUKEwjrkPqV6IqAAxVCTqQEHYEkDuIQ_BJ6BAhPEAA&ved=2ahUKEwjrkPqV6IqAAxVCTqQEHYEkDuIQ_BJ6BAhjEAg")}>
+          <Text style={styles.link}>Ubicación</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => handleLinkPress("https://www.thefork.es/restaurante/gascona-r726404#date=2023-07-13&hour=1230&partySize=2")}>
+          <Text style={styles.link}>Reserva una mesa</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => handleLinkPress("https://www.tripadvisor.es/Restaurant_Review-g187443-d23788283-Reviews-Gascona-Seville_Province_of_Seville_Andalucia.html")}>
+          <Text style={styles.link}>Estrellas de TripAdvisor</Text>
+        </TouchableOpacity>
+      </View>
+    <View style={styles.estab}>
+        <Text style={styles.bar}>SOL Y SOMBRA</Text>
+        <TouchableOpacity onPress={() => handleLinkPress("https://www.google.com/maps/reserve/v/dine/c/WfmIeBEOcrQ?source=pa&hl=es-ES&gei=1XmvZJrxI-btkdUPo760uAc&sourceurl=http://www.google.es/service/MapsPlaceService/GetPlace?authuser%3D0%26gl%3Des%26hl%3Des%26q%3DTaberna%2BSol%2By%2BSombra")}>
+          <Text style={styles.link}>Ubicación</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => handleLinkPress("https://www.thefork.es/restaurante/sol-y-sombra-r17946#date=2023-07-13&hour=1260&partySize=2")}>
+          <Text style={styles.link}>Reserva una mesa</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => handleLinkPress("https://www.tripadvisor.es/Restaurant_Review-g187443-d1153685-Reviews-Sol_y_Sombra-Seville_Province_of_Seville_Andalucia.html")}>
+          <Text style={styles.link}>Estrellas de TripAdvisor</Text>
+        </TouchableOpacity>
+      </View>
+    <View style={styles.estab}>
+        <Text style={styles.bar}>CASA REMESAL</Text>
+        <TouchableOpacity onPress={() => handleLinkPress("https://www.google.es/maps/place/Bar+Casa+Remesal/@37.3837026,-6.0009529,15z/data=!4m2!3m1!1s0x0:0xd1d4aff642c0d6fe?sa=X&ved=2ahUKEwiF3Kyd6oqAAxWzVaQEHa01Af0Q_BJ6BAhOEAA&ved=2ahUKEwiF3Kyd6oqAAxWzVaQEHa01Af0Q_BJ6BAhgEAg")}>
+          <Text style={styles.link}>Ubicación</Text>
+        </TouchableOpacity>
+        <TouchableOpacity >
+          <Text style={styles.link}>Reserva: 608 61 58 46</Text>
+        </TouchableOpacity>
+        <TouchableOpacity >
+          <Text style={styles.link}>Estrellas de TripAdvisor</Text>
+        </TouchableOpacity>
+      </View>
+    <View style={styles.estab}>
+        <Text style={styles.bar}>EL ZERO 10</Text>
+        <TouchableOpacity onPress={() => handleLinkPress("https://www.tripadvisor.es/LocationPhotoDirectLink-g187443-d8084486-i237736940-41cero10-Seville_Province_of_Seville_Andalucia.html")}>
+          <Text style={styles.link}>Ubicación</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => handleLinkPress("https://elzero10.com/reservas/")}>
+          <Text style={styles.link}>Reserva una mesa</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => handleLinkPress("https://www.tripadvisor.es/LocationPhotoDirectLink-g187443-d8084486-i237736940-41cero10-Seville_Province_of_Seville_Andalucia.html")}>
+          <Text style={styles.link}>Estrellas de TripAdvisor</Text>
+        </TouchableOpacity>
+      </View>
+
+    </ScrollView>
   );
 };
 
--- componentes/Restaurantes.js
+++ componentes/Restaurantes.js
@@ -9,42 +9,151 @@ const Restaurantes = () => {
   };
   return (
     <View style={styles.container}>
-    <View style={styles.estab}>
-      <Text style={styles.bar}>LA PARRALA</Text>
-      <TouchableOpacity onPress={() => handleLinkPress("https://www.google.es/maps/place/La+Parrala/@37.3824545,-6.0019085,15z/data=!4m5!3m4!1s0x0:0xd99fca132859003!8m2!3d37.3824545!4d-6.0019085")}>
-        <Text style={styles.link}>Ubicación</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => handleLinkPress("https://www.thefork.es/restaurante/la-parrala-r731694#booking")}>
-        <Text style={styles.link}>Reserva una mesa</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => handleLinkPress("https://www.aeat/wlpl/RHCS-JDIT/HorarioOVP")}>
-        <Text style={styles.link}>Estrellas de TripAdvisor</Text>
-      </TouchableOpacity>
-    </View>
-    <View style={styles.estab}>
-      <Text style={styles.bar}>LAS GOLONDRINAS 2</Text>
-      <TouchableOpacity onPress={() => handleLinkPress("https://www.google.es/maps/place/Las+Golondrinas+-+Pag%C3%A9s+del+Corro/@37.3840763,-6.0055876,15z/data=!4m5!3m4!1s0x0:0x9532523f76e64bd0!8m2!3d37.3840811!4d-6.0055642")}>
-        <Text style={styles.link}>Ubicación</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => handleLinkPress("")}>
-        <Text style={styles.link}>Reserva una mesa</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => handleLinkPress("https://www.aeat/wlpl/RHCS-JDIT/HorarioOVP")}>
-        <Text style={styles.link}>Estrellas de TripAdvisor</Text>
-      </TouchableOpacity>
-    </View>
-    <View style={styles.estab}>
-      <Text style={styles.bar}>EL ALBOROQUE</Text>
-      <TouchableOpacity onPress={() => handleLinkPress("https://github.com/sigleto/paginaweb.git")}>
-        <Text style={styles.link}>Ubicación</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => handleLinkPress("https://www.google.com/maps/reserve/v/dine/c/nqeAimyFvos?source=pa&hl=es-ES&gei=epXqY8raCoSCkdUPoI6N2A8&sourceurl=http://www.google.es/service/MapsPlaceService/GetPlace?authuser%3D0%26gl%3Des%26hl%3Des%26q%3DAlboroque%2B,Tapas,%2BBar%2BRestaurantes")}>
-        <Text style={styles.link}>Reserva una mesa</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => handleLinkPress("https://www.tripadvisor.co/ShowUserReviews-g187443-d1002508-r785585866-Restaurante_Alboroque-Seville_Province_of_Seville_Andalucia.html")}>
-        <Text style={styles.link}>Estrellas de TripAdvisor</Text>
-      </TouchableOpacity>
-    </View>
+<View style={styles.estab}>
+    <Text style={styles.bar}>VICTORIA 8</Text>
+    <TouchableOpacity onPress={() => handleLinkPress("https://www.google.es/maps/place/Restaurante+Victoria+8/@37.3830354,-6.0025717,15z/data=!4m5!3m4!1s0x0:0x8f72e73773904258!8m2!3d37.3830832!4d-6.0027072")}>
+      <Text style={styles.link}>Ubicación</Text>
+    </TouchableOpacity>
+    <TouchableOpacity onPress={() => handleLinkPress("https://module.thefork.com/es_ES/module/21282-fa7d6/9390-6aa?tracking_id=a93bf61a-ad12-4368-aa70-2740bd12da88")}>
+      <Text style={styles.link}>Reserva una mesa</Text>
+    </TouchableOpacity>
+    <TouchableOpacity onPress={() => handleLinkPress("https://www.tripadvisor.es/Restaurant_Review-g187443-d2261643-Reviews-Victoria_8-Seville_Province_of_Seville_Andalucia.html")}>
+      <Text style={styles.link}>Estrellas de TripAdvisor</Text>
+    </TouchableOpacity>
+  </View>
+<View style={styles.estab}>
+    <Text style={styles.bar}>CAS MONTALVAN</Text>
+    <TouchableOpacity onPress={() => handleLinkPress("https://www.google.es/maps/place/Alfarer%C3%ADa+21+-+Casa+Montalv%C3%A1n/@37.385251,-6.0060092,18z/data=!4m5!3m4!1s0xd126dcc91390a41:0x822a8a2a0246d830!8m2!3d37.3847906!4d-6.0049599?hl=es&authuser=0")}>
+      <Text style={styles.link}>Ubicación</Text>
+    </TouchableOpacity>
+    <TouchableOpacity >
+      <Text style={styles.link}>Reserva: Tfno 955 83 48 75 </Text>
+    </TouchableOpacity>
+    <TouchableOpacity onPress={() => handleLinkPress("https://www.tripadvisor.es/Restaurant_Review-g187443-d14953628-Reviews-Alfareria_21-Seville_Province_of_Seville_Andalucia.html")}>
+      <Text style={styles.link}>Estrellas de TripAdvisor</Text>
+    </TouchableOpacity>
+  </View>
+<View style={styles.estab}>
+    <Text style={styles.bar}>MARIA TRIFULCA</Text>
+    <TouchableOpacity onPress={() => handleLinkPress("https://www.tripadvisor.es/Restaurant_Review-g187443-d14953628-Reviews-Alfareria_21-Seville_Province_of_Seville_Andalucia.html")}>
+      <Text style={styles.link}>Ubicación</Text>
+    </TouchableOpacity>
+    <TouchableOpacity onPress={() => handleLinkPress("https://mariatrifulca.com/#reservas")}>
+      <Text style={styles.link}>Reserva una mesa</Text>
+    </TouchableOpacity>
+    <TouchableOpacity onPress={() => handleLinkPress("https://www.tripadvisor.es/Restaurant_Review-g187443-d11549017-Reviews-Mariatrifulca-Seville_Province_of_Seville_Andalucia.html")}>
+      <Text style={styles.link}>Estrellas de TripAdvisor</Text>
+    </TouchableOpacity>
+  </View>
+<View style={styles.estab}>
+    <Text style={styles.bar}>LA PRIMERA DEL PUENTE</Text>
+    <TouchableOpacity onPress={() => handleLinkPress("")}>
+      <Text style={styles.link}>Ubicación</Text>
+    </TouchableOpacity>
+    <TouchableOpacity onPress={() => handleLinkPress("")}>
+      <Text style={styles.link}>Reservas: Tfno 954276918</Text>
+    </TouchableOpacity>
+    <TouchableOpacity onPress={() => handleLinkPress("https://www.tripadvisor.es/Restaurant_Review-g187443-d23498957-Reviews-La_Primera_del_Puente-Seville_Province_of_Seville_Andalucia.html")}>
+      <Text style={styles.link}>Estrellas de TripAdvisor</Text>
+    </TouchableOpacity>
+  </View>
+<View style={styles.estab}>
+    <Text style={styles.bar}>RESTAURANTE EL SELLA</Text>
+    <TouchableOpacity onPress={() => handleLinkPress("https://www.google.es/maps/place/Restaurante+El+Sella+Triana+(Sevilla)/@37.384839,-6.0055886,567m/data=!3m1!1e3!4m5!3m4!1s0xd126d623bfa8ddb:0x828b4d5913d3f96f!8m2!3d37.3848113!4d-6.0027415?hl=es&authuser=0")}>
+      <Text style={styles.link}>Ubicación</Text>
+    </TouchableOpacity>
+    <TouchableOpacity onPress={() => handleLinkPress("https://www.thefork.es/restaurante/restaurante-tapas-el-sella-r584393/menu")}>
+      <Text style={styles.link}>Reserva una mesa</Text>
+    </TouchableOpacity>
+    <TouchableOpacity onPress={() => handleLinkPress("https://www.tripadvisor.es/Restaurant_Review-g187443-d15534171-Reviews-Restaurante_tapas_El_Sella-Seville_Province_of_Seville_Andalucia.html")}>
+      <Text style={styles.link}>Estrellas de TripAdvisor</Text>
+    </TouchableOpacity>
+  </View>
+<View style={styles.estab}>
+    <Text style={styles.bar}>EL PANZÓN</Text>
+    <TouchableOpacity onPress={() => handleLinkPress("https://www.google.es/maps/place/Bar+restaurante+El+Panzon/@37.3808831,-6.0149063,15z/data=!4m9!1m2!2m1!1srestaurantes!3m5!1s0xd126d39f18fe35d:0xccd78392e8a9c9ee!8m2!3d37.3754072!4d-6.0048813!15sCgxyZXN0YXVyYW50ZXNaDiIMcmVzdGF1cmFudGVzkgENYmFyX2FuZF9ncmlsbOABAA?hl=es&authuser=0")}>
+      <Text style={styles.link}>Ubicación</Text>
+    </TouchableOpacity>
+    <TouchableOpacity onPress={() => handleLinkPress("https://www.elpanzon.es/reserva/")}>
+      <Text style={styles.link}>Reserva una mesa</Text>
+    </TouchableOpacity>
+    <TouchableOpacity onPress={() => handleLinkPress("https://www.tripadvisor.es/Restaurant_Review-g187443-d17593568-Reviews-Restaurante_El_Panzon-Seville_Province_of_Seville_Andalucia.html")}>
+      <Text style={styles.link}>Estrellas de TripAdvisor</Text>
+    </TouchableOpacity>
+  </View>
+<View style={styles.estab}>
+    <Text style={styles.bar}>RINCONETE Y CORTADILLO</Text>
+    <TouchableOpacity onPress={() => handleLinkPress("https://www.google.es/maps/place/Restaurante+RICO+Rinconete+y+Cortadillo/@37.3835762,-6.0051456,401m/data=!3m1!1e3!4m9!1m2!2m1!1srestaurantes!3m5!1s0xd126d7a8b45d8b3:0xe654e565a7099026!8m2!3d37.3839673!4d-6.0045634!15sCgxyZXN0YXVyYW50ZXNaDiIMcmVzdGF1cmFudGVzkgEKcmVzdGF1cmFudJoBJENoZERTVWhOTUc5blMwVkpRMEZuU1VNMk5qUm1kRzlCUlJBQuABAA?hl=es&authuser=0")}>
+      <Text style={styles.link}>Ubicación</Text>
+    </TouchableOpacity>
+    <TouchableOpacity onPress={() => handleLinkPress("https://ricorinconeteycortadillo.com/")}>
+      <Text style={styles.link}>Reserva una mesa</Text>
+    </TouchableOpacity>
+    <TouchableOpacity onPress={() => handleLinkPress("https://www.tripadvisor.es/Restaurant_Review-g187443-d23482813-Reviews-Rico_rinconete_Y_Cortadillo-Seville_Province_of_Seville_Andalucia.html")}>
+      <Text style={styles.link}>Estrellas de TripAdvisor</Text>
+    </TouchableOpacity>
+  </View>
+<View style={styles.estab}>
+    <Text style={styles.bar}>LA VALIENTE</Text>
+    <TouchableOpacity onPress={() => handleLinkPress("https://www.google.es/maps/place/Restaurante+La+Valiente/@37.3846631,-6.0068482,674m/data=!3m1!1e3!4m9!1m2!2m1!1srestaurantes!3m5!1s0xd126d5178eef11d:0x971ff821e0cb903f!8m2!3d37.3847009!4d-6.0041434!15sCgxyZXN0YXVyYW50ZXNaDiIMcmVzdGF1cmFudGVzkgEKcmVzdGF1cmFudJoBJENoZERTVWhOTUc5blMwVkpRMEZuU1VObE5EVkRiVFJSUlJBQuABAA?hl=es&authuser=0")}>
+      <Text style={styles.link}>Ubicación</Text>
+    </TouchableOpacity>
+    <TouchableOpacity >
+      <Text style={styles.link}>Reserva: Tfno:  955 46 73 00</Text>
+    </TouchableOpacity>
+    <TouchableOpacity onPress={() => handleLinkPress("")}>
+      <Text style={styles.link}>Estrellas de TripAdvisor</Text>
+    </TouchableOpacity>
+  </View>
+<View style={styles.estab}>
+    <Text style={styles.bar}>ARROCERÍA CRIAÍTO</Text>
+    <TouchableOpacity onPress={() => handleLinkPress("https://www.google.es/maps/place/Arrocer%C3%ADa+Criaito+Triana/@37.3856463,-6.0032357,15z/data=!4m2!3m1!1s0x0:0x1a797f02f63d895e?sa=X&ved=2ahUKEwiogObB_YuAAxXaiP0HHZo_BdcQ_BJ6BAhOEAA&ved=2ahUKEwiogObB_YuAAxXaiP0HHZo_BdcQ_BJ6BAhqEAg")}>
+      <Text style={styles.link}>Ubicación</Text>
+    </TouchableOpacity>
+    <TouchableOpacity onPress={() => handleLinkPress("")}>
+      <Text style={styles.link}>Reserva: Tfno 680 70 84 39</Text>
+    </TouchableOpacity>
+    <TouchableOpacity onPress={() => handleLinkPress("https://www.tripadvisor.es/Restaurant_Review-g187443-d10721760-Reviews-Arroceria_Criaito_Triana-Seville_Province_of_Seville_Andalucia.html")}>
+      <Text style={styles.link}>Estrellas de TripAdvisor</Text>
+    </TouchableOpacity>
+  </View>
+<View style={styles.estab}>
+    <Text style={styles.bar}>SAHONA TRIANA</Text>
+    <TouchableOpacity onPress={() => handleLinkPress("https://www.google.es/maps/place/Saona+Triana/@37.3829696,-6.0045468,15z/data=!4m2!3m1!1s0x0:0xabbd62ee206cc792?sa=X&ved=2ahUKEwjDzNPe_ouAAxXziv0HHfbnAc0Q_BJ6BAhLEAA&ved=2ahUKEwjDzNPe_ouAAxXziv0HHfbnAc0Q_BJ6BAhhEAg")}>
+      <Text style={styles.link}>Ubicación</Text>
+    </TouchableOpacity>
+    <TouchableOpacity onPress={() => handleLinkPress("")}>
+      <Text style={styles.link}>Reserva: Tfno 954 66 76 04</Text>
+    </TouchableOpacity>
+    <TouchableOpacity onPress={() => handleLinkPress("https://www.tripadvisor.es/Restaurant_Review-g187443-d23685089-Reviews-Saona_Triana-Seville_Province_of_Seville_Andalucia.html")}>
+      <Text style={styles.link}>Estrellas de TripAdvisor</Text>
+    </TouchableOpacity>
+  </View>
+<View style={styles.estab}>
+    <Text style={styles.bar}>ASADOR ATAHUALPA</Text>
+    <TouchableOpacity onPress={() => handleLinkPress("https://www.google.es/maps/place/Atahualpa/@37.3789583,-6.0009944,15z/data=!4m2!3m1!1s0x0:0x95cb20765fe398b?sa=X&ved=2ahUKEwjJxubm_4uAAxWWtqQKHZgKAR8Q_BJ6BAhHEAA&ved=2ahUKEwjJxubm_4uAAxWWtqQKHZgKAR8Q_BJ6BAhoEAg")}>
+      <Text style={styles.link}>Ubicación</Text>
+    </TouchableOpacity>
+    <TouchableOpacity onPress={() => handleLinkPress("https://www.thefork.es/restaurante/parrilla-argentina-atahualpa-r14396")}>
+      <Text style={styles.link}>Reserva una mesa</Text>
+    </TouchableOpacity>
+    <TouchableOpacity onPress={() => handleLinkPress("https://www.tripadvisor.es/Restaurant_Review-g187443-d4433546-Reviews-Atahualpa-Seville_Province_of_Seville_Andalucia.html")}>
+      <Text style={styles.link}>Estrellas de TripAdvisor</Text>
+    </TouchableOpacity>
+  </View>
+<View style={styles.estab}>
+    <Text style={styles.bar}>MESÓN CASA LUCIANO</Text>
+    <TouchableOpacity onPress={() => handleLinkPress("https://www.google.es/maps/place/Mes%C3%B3n+Casa+Luciano/@37.3794444,-6.0008333,15z/data=!4m2!3m1!1s0x0:0x8fc31191f572feeb?sa=X&ved=2ahUKEwigg8jKgIyAAxXLGewKHbRgCrQQ_BJ6BAhLEAA&ved=2ahUKEwigg8jKgIyAAxXLGewKHbRgCrQQ_BJ6BAhlEAg")}>
+      <Text style={styles.link}>Ubicación</Text>
+    </TouchableOpacity>
+    <TouchableOpacity onPress={() => handleLinkPress("https://www.thefork.es/restaurante/meson-casa-luciano-r36392")}>
+      <Text style={styles.link}>Reserva una mesa</Text>
+    </TouchableOpacity>
+    <TouchableOpacity onPress={() => handleLinkPress("https://www.tripadvisor.es/Restaurant_Review-g187443-d3876780-Reviews-Meson_Casa_Luciano-Seville_Province_of_Seville_Andalucia.html")}>
+      <Text style={styles.link}>Estrellas de TripAdvisor</Text>
+    </TouchableOpacity>
+  </View>
+
   </View>
 );
 };
